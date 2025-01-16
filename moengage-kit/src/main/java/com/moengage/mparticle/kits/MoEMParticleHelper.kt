722e47dfc52b7472c1d18edffd07da9af51575e7
Umang Chamaria <umang@moengage.com>
2025-01-16 13:37:52 +0530

    documentation

--- moengage-kit/src/main/java/com/moengage/mparticle/kits/MoEMParticleHelper.kt
+++ moengage-kit/src/main/java/com/moengage/mparticle/kits/MoEMParticleHelper.kt
@@ -1,10 +1,19 @@
 package com.moengage.mparticle.kits
 
+import android.app.Application
 import com.moengage.mparticle.kits.internal.Cache
 import com.mparticle.MParticle.IdentityType
 
+/**
+ * Helper class for setting up MoEngage specific configurations for mParticle<>MoEngage SDK.
+ */
 public object MoEMParticleHelper {
 
+    /**
+     * Set the readable [String] mapping for [IdentityType].
+     *
+     * Ensure this mapping set before identifying the user in the mParticle SDK. Possibly call this method in the onCreate() of  [Application] class.
+     */
     public fun setMappingForIdentity(mapping: Map<IdentityType, String>) {
         Cache.identityMapping = mapping
     }
